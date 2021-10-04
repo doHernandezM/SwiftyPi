021846136628fb2c2c1878cea7b6014408917de5
doHernandez <dennis@dohernandez.net>
2021-10-03 20:12:59 -0700

    Equatable, Hashable

--- Sources/SwiftyPi/Rules/Rules.Base.swift
+++ Sources/SwiftyPi/Rules/Rules.Base.swift
@@ -8,7 +8,7 @@
 import Foundation
 //import Combine
 
-open class Rules {
+open class RuleController {
     public static var rules:[Rule] = []
     
     public init() {
@@ -16,9 +16,18 @@ open class Rules {
     }
 }
 
-open class Rule: ObservableObject {
+open class Rule: ObservableObject, Hashable, Equatable {
+    @Published public var id: UUID = UUID()
     @Published public var inputCondition: [RuleConditional]? = nil
     @Published public var outputCondition: [RuleConditional]? = nil
+    
+    public static func == (lhs: Rule, rhs: Rule) -> Bool {
+        if lhs.id == rhs.id { return true}
+        return false
+    }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.id)
+    }
 }
 
 public protocol RuleConditional {
